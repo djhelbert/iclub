591c54ccbecac9b0edcb7c95a6e932dd7dcc2d86
djhel <djhel@DESKTOP-AVKQL7I>
2017-03-17 22:44:48 -0700

    Added Servlet

---
+++ src/main/java/org/iclub/config/ServletConfig.java
@@ -0,0 +1,18 @@
+package org.iclub.config;
+
+import org.iclub.servlet.BinaryFileServlet;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication
+public class ServletConfig {
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean(BinaryFileServlet binaryFileServlet) {
+	    return new ServletRegistrationBean(binaryFileServlet, "/file");
+	}
+
+}
---
+++ src/main/java/org/iclub/servlet/BinaryFileServlet.java
@@ -0,0 +1,63 @@
+package org.iclub.servlet;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.iclub.model.BinaryFile;
+import org.iclub.service.BinaryFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinaryFileServlet extends HttpServlet {
+
+	private BinaryFileService binaryFileService;
+
+	private static final long serialVersionUID = -8674792365261419577L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(BinaryFileServlet.class);
+
+	@Autowired
+	public BinaryFileServlet(BinaryFileService binaryFileService) {
+		super();
+		this.binaryFileService = binaryFileService;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		final String id = req.getParameter("id");
+
+		BinaryFile binaryFile = binaryFileService.getBinaryFile(Long.getLong(id));
+		resp.setContentType(binaryFile.getMimetype());
+
+		final ByteArrayInputStream is = new ByteArrayInputStream(binaryFile.getData());
+		OutputStream out = null;
+
+		try {
+			out = resp.getOutputStream();
+			byte[] buf = new byte[binaryFile.getData().length];
+		    int count = 0;
+
+			while ((count = is.read(buf)) >= 0) {
+			     out.write(buf, 0, count);
+			}
+		} catch (IOException e) {
+			LOGGER.error("BinaryFileServlet.doGet()", e);
+		}
+
+	    try {
+	    	if (out != null) {
+			  out.close();
+	    	}
+		} catch (IOException e) {
+			LOGGER.error("BinaryFileServlet.doGet()", e);
+		}
+	}
+	
+}
