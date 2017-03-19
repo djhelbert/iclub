1ea4c37db85f0beac9a23c5d4b2c26a23647b4f6
djhel <djhel@DESKTOP-AVKQL7I>
2017-03-18 22:02:22 -0700

    Added unit test

---
+++ src/test/java/org/iclub/service/ContentServiceTest.java
@@ -0,0 +1,32 @@
+package org.iclub.service;
+
+import java.util.Optional;
+
+import org.iclub.model.Content;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ContentServiceTest {
+
+    @Autowired
+    private ContentService contentService;
+
+    @Test
+    public void testService() {
+    	contentService.save(getContent());
+    	Optional<Content> optional = contentService.getContentByName(ContentService.ABOUT);
+    	assert optional.isPresent();
+    }
+
+    private Content getContent() {
+    	final Content content = new Content();
+    	content.setName(ContentService.ABOUT);
+    	content.setContentText("Text.....");
+    	return content;
+    }
+}
